5b6d7398925b9fe0f6ab11563f8b93f99cf86650
andrzejsliwa <andrzej.sliwa@i-tool.eu>
2011-04-14 13:36:31 +0200

    added erlang version

--- threads-compo/README.md
+++ threads-compo/README.md
@@ -10,3 +10,24 @@ To run the example call the following in console:
     ruby threads.rb
 
 Running example on 100000 elements hanged up my PC _(ruby 1.8.7 patch 72)_, when limited to 10000 threads it runs in 16 seconds.
+
+### Erlang version - both cases (compiled vs interpreted as escript)
+
+Running interpreded version via escript (uncommend first line for this test)
+    $./processes.erl
+    Process spawn time = 22961 ms
+    Bye!
+    ...
+
+Running comiled version
+
+    $erl +P 200000
+    1> c(processes).
+    ok
+    2> processes:main([]).
+    Process spawn time = 740 ms
+    ok
+    Bye!
+    ...
+
+So ... Interpreted erlang via escript (not compiled) is slower than compiled version (what is obvious)... and is as slow as java (compiled ... what is also obvoius ;) which using native threads.
---
+++ threads-compo/processes.erl
@@ -0,0 +1,42 @@
+%%#!/usr/bin/env escript
+%%
+%% Running interpreded version via escript (uncommend first line for this test)
+%%     $./processes.erl
+%%     Process spawn time = 22961 ms
+%%     Bye!
+%%     ...
+%%
+%% Running comiled version
+%%
+%%     $erl +P 200000
+%%     1> c(processes).
+%%     ok
+%%     2> processes:main([]).
+%%     Process spawn time = 740 ms
+%%     ok
+%%     Bye!
+%%     ...
+%%
+%% So ... Interpreted erlang via escript (not compiled) is slower
+%% than compiled version (what is obvious)... and is as slow
+%% as java (compiled ... what is also obvoius ;) which
+%% using native threads.
+
+-module(processes).
+-export([main/1]).
+
+main(_) ->
+  statistics(wall_clock),
+  for(1, 100000, fun() -> spawn(fun() -> wait() end) end),
+  {_, Result} = statistics(wall_clock),
+  io:format("Process spawn time = ~p ms~n", [Result]).
+
+wait() ->
+  receive
+  after 1000 ->
+    io:format("Bye!~n"),
+    true
+  end.
+
+for(To, To, Fun) -> [ Fun() ];
+for(From, To, Fun) -> [ Fun() | for(From + 1, To, Fun)].
