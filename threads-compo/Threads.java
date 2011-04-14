1f3e7c63dc0ca556a51d23a163234e5cedfab114
Michal Orman <michal.orman@gmail.com>
2011-04-14 11:57:14 +0200

    Updated threads-compo java solution.

--- threads-compo/Threads.java
+++ threads-compo/Threads.java
@@ -1,29 +1,25 @@
 public class Threads {
 
-    static class HelloThread implements Runnable {
-        public void run() { 
-            System.out.println("Hello world");
-            try {
-                Thread.sleep(1000);
-            } catch (Exception ex) { 
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    public static void main(String... args) throws Exception {
+    public static void main(String... args) {
 
         long start = System.currentTimeMillis();
 
         for (int i = 0; i < 100000; i++) {
-            Thread thread = new Thread(new HelloThread());
-            thread.start();
+            new Thread(new Runnable() {
+                    public void run() { 
+                        System.out.println("Hello world");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception ex) { 
+                            ex.printStackTrace();
+                        }
+                    }
+                }).start();
         }
 
         long stop = System.currentTimeMillis();
 
         System.out.println("Executed in: " + (stop - start) + "ms");
-
     }
 
 }
